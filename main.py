68daec9208409a880adef7e37616a094cd4af072
dsukh22 <hzchenapisa@gmail.com>
2024-11-21 02:46:10 +0700

    Итерация 1

---
+++ main.py
@@ -0,0 +1,14 @@
+def Print_Menu():
+    return "образец"
+
+
+def String_Work():
+    return "образец"
+
+
+def Encryption_Process():
+    return "образец"
+
+
+def Printing_A_Result():
+    return "образец"
